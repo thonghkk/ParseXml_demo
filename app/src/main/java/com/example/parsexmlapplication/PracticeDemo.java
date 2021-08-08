a9808bffd7153a4eb40f0659dbc9c520d59f844d
thonghkk <thongnv@iotlink.com.vn>
2021-08-08 23:49:12 +0700

    practice demo part 1

---
+++ app/src/main/java/com/example/parsexmlapplication/PracticeDemo.java
@@ -0,0 +1,14 @@
+package com.example.parsexmlapplication;
+
+public class PracticeDemo {
+    void check(){
+        String b = "12" ;
+    }
+    
+    void checkAgain(){
+        Integer a = 12;
+    }
+    void check2(){
+        String c = "1231";
+    }
+}
